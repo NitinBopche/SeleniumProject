5c5c9e335671bf2a93b34ef5fc10a736cee9c7a2
NitinBopche <nitinbopche1994@gmail.com>
2023-06-13 17:49:46 +0530

    Uploaded Selenium project

---
+++ EclipsWorkspace/MyFrame/src/test/java/Utilities/FrameworkHelper.java
@@ -0,0 +1,53 @@
+package Utilities;
+
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+public class FrameworkHelper extends BaseClass{
+
+	public void GenericDropdown(List<WebElement> WE, String value) {
+		int length = WE.size();
+		for(int i=0; i<length; i++) {
+			if(WE.get(i).getText().equals(value)) {
+				WE.get(i).click();
+				break;
+			}
+		}
+	}
+
+	public void dragAndDrop(WebDriver driver, WebElement srcElement, WebElement tarElement) {
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
+				+ "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n" + "data: {},\n"
+				+ "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
+				+ "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n" + "return event;\n"
+				+ "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
+				+ "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
+				+ "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
+				+ "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n" + "}\n"
+				+ "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
+				+ "var dragStartEvent =createEvent('dragstart');\n" + "dispatchEvent(element, dragStartEvent);\n"
+				+ "var dropEvent = createEvent('drop');\n"
+				+ "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
+				+ "var dragEndEvent = createEvent('dragend');\n"
+				+ "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
+				+ "var source = arguments[0];\n" + "var destination = arguments[1];\n"
+				+ "simulateHTML5DragAndDrop(source,destination);", srcElement, tarElement);
+	}
+
+	public void Click(WebElement webelement) {
+		wait.until(ExpectedConditions.elementToBeClickable(webelement));
+		webelement.click();
+	}
+	
+	public String GetText(WebElement webelement) {
+		wait.until(ExpectedConditions.visibilityOf(webelement));
+		return webelement.getText();
+	}
+
+}
